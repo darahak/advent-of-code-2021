c86797d5466058d321a94a2c89ae23fe5a478325
Dara Hak <dara.hak@gmail.com>
2022-01-01 18:17:19 +0100

    Day 1

--- day1/index.js
+++ day1/index.js
@@ -2,10 +2,45 @@ export default {
   /** @argument input {string} */
   first(input) {
     const lines = input.trim().split('\n');
+    const values = lines.map((line) => Number.parseInt(line));
+
+    let increaseCount = 0;
+    let previous = values.shift();
+
+    values.forEach((value) => {
+      if (value > previous) {
+        ++increaseCount;
+      }
+
+      previous = value;
+    });
+
+    return increaseCount;
   },
 
   /** @argument input {string} */
   second(input) {
     const lines = input.trim().split('\n');
+    const values = lines.map((line) => Number.parseInt(line));
+
+    let increaseCount = -1;
+    let previous = 0;
+
+    values.forEach((value, index) => {
+      const a = values[index + 1];
+      const b = values[index + 2];
+
+      if (a != null && b != null) {
+        const sum = value + a + b;
+
+        if (sum > previous) {
+          ++increaseCount;
+        }
+
+        previous = sum;
+      }
+    });
+
+    return increaseCount;
   },
 };
